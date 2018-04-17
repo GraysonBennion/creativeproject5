da882688c4d9a27fce3aac6c0c22cca4e1272aa0
Grayson Bennion <graysonbennion@gmail.com>
2018-04-17 14:51:30 -0600

    Added page that will allow a user to edit specific decks

---
+++ src/components/EditPage.vue
@@ -0,0 +1,29 @@
+<template> 
+  <div class="feed">
+    <h2>The Edit Page!</h2>
+    <p>{{text}}</p>
+  </div>
+</template>
+
+
+<script>
+import moment from 'moment';
+ export default {
+   name: 'EditPage',
+   created: function() {
+     
+   },
+   computed: {
+     text: function() {
+        return this.$route.query.text;
+     }
+   },
+   watch: {
+     
+   },
+ }
+</script>
+
+<style scoped>
+ 
+</style>
